a9965f80d2c621c1ed06116e7757d3b2def24502
Ayushmaan Padhi <ayushmaanpadhi2805@gmail.com>
2023-10-06 09:19:33 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -141,7 +141,7 @@ fi
 echo ""
 read -r -p "Do you want TLP? [y/N] " response
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    sudo pacman -S pacman -S tlp tlp-rdw
+    sudo pacman -S tlp tlp-rdw
     sudo systemctl enable tlp.service
     sudo systemctl enable NetworkManager-dispatcher.service
     sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
